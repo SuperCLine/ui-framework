90515567f26d9999b80d2c02b736e57799435553
xfanhsl <xfanhsl@gmail.com>
2020-04-19 13:11:05 +0800

    + add lua debug.

--- LuaProject/script/init.lua
+++ LuaProject/script/init.lua
@@ -3,6 +3,7 @@ print("------------- init lua -------------")
 supercline = {}
 supercline.Prefab = {}
 
+require("script.util.LuaPanda").start("127.0.0.1",8818);
 require("script.cs")
 require("script.load")
 require("script.bridge")
